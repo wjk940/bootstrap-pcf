ee41e6a30b6221e62e289d612d308093c43269dc
wjk940 <github@wjkeenan.org>
2017-05-02 22:12:22 -0700

    changes after "team dogfooding"

--- azure/mkboshadmin.sh
+++ azure/mkboshadmin.sh
@@ -2,25 +2,25 @@
 BOSH=`which bosh2`
 BOSH_DEPLOYMENT=bosh-deployment
 
-# Deploy a Director -- ./creds.yml is generated automatically
+# Deploy a Director -- ./bosh-creds.yml is generated automatically
 echo "create-env"
 ${BOSH} create-env ${BOSH_DEPLOYMENT}/bosh.yml \
   --state ./state.json \
   -o ${BOSH_DEPLOYMENT}/azure/cpi.yml \
-  --vars-store ./creds.yml \
-  -l  ./bootstrap-azure-pcf.yml
+  --vars-store ./bosh-creds.yml \
+  -l [MY FILLED OUT PARAM.YML]
 
 export BOSH_ENVIRONMENT=boshadmin
 
 # Alias deployed Director
 echo "alias-env"
-${BOSH} -e 10.0.0.8 --ca-cert <(${BOSH} int ./creds.yml --path /director_ssl/ca) alias-env ${BOSH_ENVIRONMENT}
+${BOSH} -e 10.0.0.8 --ca-cert <(${BOSH} int ./bosh-creds.yml --path /director_ssl/ca) alias-env ${BOSH_ENVIRONMENT}
 
 # Log in to the Director
 export BOSH_CLIENT=admin
-export BOSH_CLIENT_SECRET=`${BOSH} int ./creds.yml --path /admin_password`
+export BOSH_CLIENT_SECRET=`${BOSH} int ./bosh-creds.yml --path /admin_password`
 
 # Update cloud config -- single az
 echo "update-cloud-config"
 ${BOSH} -n update-cloud-config ${BOSH_DEPLOYMENT}/azure/cloud-config.yml \
-  -l  ./bootstrap-azure-pcf.yml
+  -l [MY FILLED OUT PARAM.YML]
